0e870d7e7acd462b14fd583cc11fe372ad3e3ca0
Peter Kibigo <54945436+ndunguesqpeter@users.noreply.github.com>
2026-06-23 13:08:46 +0300

    "Add exercise on loops"

--- Part3/test.py
+++ Part3/test.py
@@ -1,10 +1,5 @@
-upper_limit = int(input("Upper limit: "))
-sum_of_numbers = 1
-number = 1
-next_number = "1"
-
-while sum_of_numbers < upper_limit:
-    number += 1
-    sum_of_numbers += number
-    next_number += f" + {number}"
-print(f"The consucutive sum : {next_number} = {sum_of_numbers}")
+num = 5
+row = 1
+while row <= num:
+    print("*" * row)
+    row += 1
